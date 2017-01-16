c28ae9e44135d2b50479e28b843ed8dc634b5484
noamiko <noamiko@gmail.com>
2017-01-16 12:15:27 +0200

    fix source for unity

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,8 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.2f2.pkg: '
-curl -o Unity.pkg http://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.2f2.pkg
+echo 'Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity.pkg -target /	
