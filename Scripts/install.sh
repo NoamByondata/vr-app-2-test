eca62c48bceb9cb92a3aa8c2700cf05d2e0cd556
noamiko <noamiko@gmail.com>
2017-01-16 16:41:56 +0200

    asdf

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -8,12 +8,10 @@ echo 'Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/M
 curl -o Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -package Unity.pkg -target /
 
 echo 'Downloading Android package from http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg: '
 curl -o Android.pkg http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg
 
-ls -l
-
 echo 'Installing Android.pkg'
-sudo installer -dumplog Android.pkg -target /
+sudo installer -package Android.pkg -target /
