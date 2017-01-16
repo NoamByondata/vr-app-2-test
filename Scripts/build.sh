97926be6e550178d8a88433040ba81fc4f7882ff
noamiko <noamiko@gmail.com>
2017-01-16 15:00:47 +0200

    try to build for android

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,15 +5,16 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="ci-build"
 
-echo "Attempting to build $project for Windows"
+echo "Attempting to build $project for Android"
+
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
+  -executeMethod BuildMyGame.BuildAndroid "$(pwd)/Build/windows/$project.apk"
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -7,5 +7,11 @@
 echo 'Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg: '
 curl -o Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
+echo 'Downloading Android package from http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg: '
+curl -o Android.pkg http://download.unity3d.com/download_unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-5.4.4f1.pkg
+
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /	
+sudo installer -dumplog -package Unity.pkg -target /
+
+echo 'Installing Android.pkg'
+sudo installer -dumplog -package Android.pkg -target /
