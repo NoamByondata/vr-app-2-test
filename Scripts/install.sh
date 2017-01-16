695683b8dd288794146c69d719916e1510a3922c
noamiko <noamiko@gmail.com>
2017-01-16 16:43:40 +0200

    asdf

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,6 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+uname -a
+
 echo 'Downloading from http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg: '
 curl -o Unity.pkg http://download.unity3d.com/download_unity/5a3967d8c55d/MacEditorInstaller/Unity-5.4.4f1.pkg
 
